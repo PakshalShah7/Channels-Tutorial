358dba46c63e4074fe7331da58069ce722b39d86
PakshalShah7 <190770116128@socet.edu.in>
2023-09-04 20:03:06 +0530

    Initial commit

---
+++ channels_tutorial/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for channels_tutorial project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+import example.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_tutorial.settings')
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(example.routing.websocket_urlpatterns))
+    )
+})
